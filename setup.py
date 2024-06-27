a35a79bfd625e776e4d64ed47ca7be308e58c3b7
ENTER YOUR NAME <Enter_your@email.com>
2024-06-27 19:20:28 +0200

    requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 # setup.py
 from setuptools import setup, find_packages
 
+def read_requirements():
+    with open('requirements.txt') as req:
+        content = req.read()
+        requirements = content.split('\n')
+    return requirements
+
 setup(
-    name='pyvectorSDK',
+    name='vectorSDK',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        'numpy'  # our package uses Numpy, here you can include other packages
-    ],
+    install_requires=read_requirements()
 )
